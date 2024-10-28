e3e09e8ff19204f3c0c07e9e0fbade11b6f3aa7a
Brunella DAnzi <brunella.danzi@cern.ch>
2024-10-28 02:30:59 -0700

    Fix hltMultiTrackValidation to accomodate NoDoubletRecovery

--- HLTrigger/Configuration/python/customizeHLTforCMSSW.py
+++ HLTrigger/Configuration/python/customizeHLTforCMSSW.py
@@ -281,8 +281,8 @@ def customizeHLTforCMSSW(process, menuType="GRun"):
     (stripNtupletFit).makeProcessModifier(customizeHLTforAlpakaStripNoDoubletRecovery).apply(process)
      
     #from Configuration.ProcessModifiers.pixelNtupletFit_cff import pixelNtupletFit
-    #from HLTrigger.Configuration.customizeHLTforAlpakaStrip import customizeHLTforAlpakaStrip
-    #(pixelNtupletFit).makeProcessModifier(customizeHLTforAlpakaStrip).apply(process)
+    from HLTrigger.Configuration.customizeHLTforAlpakaStrip import customizeHLTforAlpakaStrip
+    #(stripNtupletFit).makeProcessModifier(customizeHLTforAlpakaStrip).apply(process)
      
      
     # add call to action function in proper order: newest last!
