2957d8dbf198477a1754acb07fc9e8044a1ac86d
Brunella DAnzi <brunella.danzi@cern.ch>
2025-01-06 07:17:21 -0800

    Squash and rebase 15_0_0_pre1

--- DQM/HcalTasks/plugins/RawTask.cc
+++ DQM/HcalTasks/plugins/RawTask.cc
@@ -431,6 +431,8 @@ std::shared_ptr<hcaldqm::Cache> RawTask::globalBeginLuminosityBlock(edm::Luminos
       for (uint32_t iflag = 0; iflag < _vflags.size(); iflag++)
         _cSummaryvsLS_FED.setBinContent(eid, _currentLS, int(iflag), int(flag::fNCDAQ));
       _cSummaryvsLS.setBinContent(eid, _currentLS, int(flag::fNCDAQ));
+      if (!hcaldqm::utilities::isFEDHO(eid) && fed != 1136)
+        _cBadQ_FEDvsLSmod10.setBinContent(eid, _currentLS % 10, int(flag::fNCDAQ));
       continue;
     }
 
