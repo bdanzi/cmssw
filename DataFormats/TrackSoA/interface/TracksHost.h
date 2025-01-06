5f1a5fa8f35c05d9b5e7136100f97c2247bd2f06
Brunella DAnzi <brunella.danzi@cern.ch>
2025-01-06 09:04:12 -0800

    Squash and rebase to 14_2_1

--- DataFormats/TrackSoA/interface/TracksHost.h
+++ DataFormats/TrackSoA/interface/TracksHost.h
@@ -3,6 +3,7 @@
 
 #include <cstdint>
 #include <alpaka/alpaka.hpp>
+#include "Geometry/CommonTopologies/interface/SimplePixelStripTopology.h"
 #include "Geometry/CommonTopologies/interface/SimplePixelTopology.h"
 #include "DataFormats/TrackSoA/interface/TracksSoA.h"
 #include "DataFormats/TrackSoA/interface/TrackDefinitions.h"
@@ -36,6 +37,7 @@ namespace pixelTrack {
   using TracksHostPhase1 = TracksHost<pixelTopology::Phase1>;
   using TracksHostPhase2 = TracksHost<pixelTopology::Phase2>;
   using TracksHostHIonPhase1 = TracksHost<pixelTopology::HIonPhase1>;
+  using TracksHostPhase1Strip = TracksHost<pixelTopology::Phase1Strip>;
 
 }  // namespace pixelTrack
 
