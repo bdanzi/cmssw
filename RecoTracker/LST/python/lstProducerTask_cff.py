dc618f224e5dbcca5c9695fadf05939fc87e863f
Brunella DAnzi <brunella.danzi@cern.ch>
2026-06-06 10:54:33 -0700

    Lowering pT thresholds for mkFit seeds input from LST, no pLS duplicate cleaning, passThrough for all tracks disabled

--- RecoTracker/LST/python/lstProducerTask_cff.py
+++ RecoTracker/LST/python/lstProducerTask_cff.py
@@ -4,4 +4,19 @@ from RecoTracker.LST.lstProducer_cfi import lstProducer
 
 from RecoTracker.LST.lstModulesDevESProducer_cfi import lstModulesDevESProducer
 
+from Configuration.ProcessModifiers.seedingLST_cff import seedingLST
+from Configuration.ProcessModifiers.trackingLST_cff import trackingLST
+from Configuration.ProcessModifiers.trackingMkFitHighPtTripletStep_cff import trackingMkFitHighPtTripletStep
+from Configuration.Eras.Modifier_trackingPhase2PU140_cff import trackingPhase2PU140
+
+(trackingPhase2PU140 & trackingMkFitHighPtTripletStep & seedingLST & trackingLST).toModify(lstProducer,
+    ptCut = cms.double(0.6),
+    ptCutLabel = cms.string('0.6'),
+    nopLSDupClean = cms.bool(True),
+    tcpLSTriplets = cms.bool(True)
+)
+(trackingPhase2PU140 & trackingMkFitHighPtTripletStep & seedingLST & trackingLST).toModify(lstModulesDevESProducer,
+    ptCutLabel = cms.string('0.6')
+)
+
 lstProducerTask = cms.Task(lstModulesDevESProducer, lstProducer)
